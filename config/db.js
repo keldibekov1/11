63b98bd58c463d6b7b2f939475c3e3de1e2174cc
O'tkir <keldibekovotkir767@gmail.com>
2025-02-05 22:43:12 +0500

    1

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mysql from "mysql2/promise";
 
 const db = await mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "1212",
   database: "world",
